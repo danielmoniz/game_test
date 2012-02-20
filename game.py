f3a459096195678f69f51b66fec4d1377ee8e026
Daniel Moniz <daniel.moniz@gmail.com>
2012-02-19 19:07:56 -0500

    Mostly complete user input function

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-import player
+import player as playerClass
 
 class Game:
     """The main game class."""
@@ -9,16 +9,19 @@ class Game:
     def __init__(self):
         pass
 
-    def get_confirm_input(question = "yes (y) or no (n)? ", confirm_answers = ["yes", "y"], decline_answers = ["no", "n"]):
-        """Loop until the person puts in a frigging integer"""
+    def get_confirm_input(self, question = "yes (y) or no (n)? ", confirm_answers = ["yes", "y"], decline_answers = ["no", "n"]):
+        """Loop until the person puts in an acceptable value. An empty list of acceptable answers indicates that any answer is okay."""
         while 1:
             try:
-                user_input = int(raw_input(question))
-                if user_input in confirm_answers:
+                user_input = raw_input(question)
+                if confirm_answers == []:
+                    return user_input
+                elif user_input in str(confirm_answers):
                     return 1;
-                elif user_input in decline_answers:
+                elif user_input in str(decline_answers):
                     return 0;
                 else:
+                    print user_input
                     raise ValueError;
             except ValueError:
                 print "Bad input!"
@@ -31,16 +34,17 @@ class Game:
         num_players = self.get_confirm_input(question, range(1, 4), [])
         players = []
         for i in range(1, num_players + 1):
-            question = "Is player human?"
+            print i
+            question = "Is player human? "
             is_human = self.get_confirm_input(question)
             if is_human:
-                name = self.get_confirm_input("Enter player name: ")
-                player = player.Player()
-                players.append(player)
+                question = "Enter player {0} name: ".format(i)
+                name = self.get_confirm_input(question, [], [])
+            else:
+                name = "Player {0}".format(i)
+            player = playerClass.Player(i, name, is_human)
+            players.append(player)
                 
-                
-
-
         return num_players, players
         
     def end_game(self):
@@ -51,6 +55,7 @@ class Game:
 
 # get initial game setup information
         num_players, players = self.get_players()
+        print players
         
         while 1:
             cmd = raw_input("Enter a command: ")
